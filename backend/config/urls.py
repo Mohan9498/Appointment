b123f445b46113f1283ee30e94e65bb18836cdc7
Mohan9498 <itzmemktech@gmail.com>
2026-03-17 13:56:37 +0530

    fixed migration issue

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls, name="admin-panel"),
 
     path("api/accounts/", include("accounts.urls")),
     path("api/appointments/", include("appointments.urls")),
